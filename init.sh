b800daacc13582b5bd286abba1dc844ad82a451b
Masaki Yasutake <my17560@gmail.com>
2024-04-06 16:39:57 +0900

    Added init.sh.

---
+++ init.sh
@@ -0,0 +1,9 @@
+cd ~/Projects/acobook_dockers/acobook_nginx/contents
+ln -s ~/Projects/bitsmist-js_v1 .
+ln -s ~/Projects/bitsmist-js-extras_v1 .
+ln -s ~/Projects/acobook_app .
+
+cd ~/Projects/acobook_dockers/acobook_phpfpm/contents
+ln -s ~/Projects/bitsmist-server_v1 .
+ln -s ~/Projects/acobook_server .
+ln -s ~/Projects/acobook_api .
