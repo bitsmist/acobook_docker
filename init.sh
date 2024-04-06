fc8465a41a6bb47af9d2047f35b70ffc070e4d5b
Masaki Yasutake <my17560@gmail.com>
2024-04-06 18:31:31 +0900

    Fixed init.sh.

--- init.sh
+++ init.sh
@@ -1,9 +1,13 @@
-cd nginx/contents
+#!/bin/bash
+
+pushd nginx/contents
 ln -s ../../../bitsmist-js_v1 .
 ln -s ../../../bitsmist-js-extras_v1 .
 ln -s ../../../acobook_app .
+popd
 
-cd phpfpm/contents
+pushd phpfpm/contents
 ln -s ../../../bitsmist-server_v1 .
 ln -s ../../../acobook_server .
 ln -s ../../../acobook_api .
+popd
