831d1aa7e5ecef90d1c0a95766f671180e6bf181
Masaki Yasutake <my17560@gmail.com>
2024-04-06 17:28:17 +0900

    Modified to use relative paths.

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
-cd ~/Projects/acobook_dockers/acobook_nginx/contents
+cd acobook_dockers/acobook_nginx/contents
 ln -s ../../../bitsmist-js_v1 .
 ln -s ../../../bitsmist-js-extras_v1 .
 ln -s ../../../acobook_app .
 
-cd ~/Projects/acobook_dockers/acobook_phpfpm/contents
+cd acobook_dockers/acobook_phpfpm/contents
 ln -s ../../../bitsmist-server_v1 .
 ln -s ../../../acobook_server .
 ln -s ../../../acobook_api .
